1c063ccbabb99f6f538f64fa3d78f18f5c141332
Julia Kowalik <Julia.Kowalik@grupawp.pl>
2022-03-08 00:44:41 +0100

    Added rstest and written datetime parsing tests

--- src/time.rs
+++ src/time.rs
@@ -30,3 +30,28 @@ impl TryInto<DateTime<Utc>> for StartFrom {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use chrono::{DateTime, Utc};
+    use rstest::rstest;
+
+    use crate::StartFrom;
+
+    #[rstest]
+    #[case("1983 Apr 13 12:09:14.274 +0000", "%Y %b %d %H:%M:%S%.3f %z")]
+    #[case("1983.04.13T12:09:14+09", "%Y.%m.%dT%H:%M:%S%#z")]
+    #[case("1983.04.13T12:09:14-09", "%Y.%m.%dT%H:%M:%S%#z")]
+    fn parse_utc_time(#[case] date: &str, #[case] format: &str) {
+        let expected = DateTime::parse_from_str(date, format).unwrap();
+
+        let parsed: DateTime<Utc> = (StartFrom::DateTime {
+            date_time: date.to_string(),
+            format: format.to_string(),
+        })
+        .try_into()
+        .unwrap();
+
+        assert_eq!(expected, parsed);
+    }
+}
